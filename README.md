c08145ce6738fe3a3a28f40c64c51c0e75b241da
Vladimir Belich <belichvladimir@mail.ru>
2024-04-22 21:21:04 +0300

    GRADE-86/belich/prototype-component

--- README.md
+++ README.md
@@ -3,4 +3,10 @@
 - после клонирования npm install
 - запуск npm run dev
 
-  Ура!!! Уведомления работают)))
+
+### JSON Server
+
+- добавляем (по аналогии с данными расположенными в файле) данные в файл ./static/db.json
+- запускаем json server командой npx json-server ./static/db.json
+- http://localhost:3000/[ключ из объекта в db.json]
+- ссылка на док-ю https://github.com/typicode/json-server/tree/v0
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-require('dotenv').config();
-import express, { Express, Request, Response } from "express";
+import dotenv from 'dotenv';
+import express from 'express';
+import type { Request, Response } from 'express';
 
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Streamo-backend ');
+  res.send('Streamo-backend');
 });
 
 app.listen(port, () => {
