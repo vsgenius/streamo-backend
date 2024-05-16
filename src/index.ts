4543bbf7e8a8799463c7eb2bc7f3fcff018f93ed
MikhailMgeb <mmd920802@gmail.com>
2024-05-16 12:54:01 +0300

    GRADE-139: linter для бэкенда

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
+
 import express from 'express';
+
 import type { Request, Response } from 'express';
 
 dotenv.config();
@@ -7,11 +9,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.get('/', (req: Request, res: Response) => {
   res.send('Streamo-backend');
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running http://localhost:${port}`);
+  return (`[server]: Server is running http://localhost:${port}`);
 });
